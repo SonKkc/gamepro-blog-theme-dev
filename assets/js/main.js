746f09d35f2b4323b40e0055dfc8ceee9b6fa167
SonKkc <soncb.xyz@gmail.com>
2024-09-25 15:06:53 +0700

    Refactor SCSS files and update Tailwind configuration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,19 @@
 
 document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('loading-screen').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
     console.log("DOM loaded");
-    const listItems = document.querySelectorAll("li");
-
-    listItems.forEach(function (li) {
-        const anchor = li.querySelector("a.active-link");
-        if (anchor) {
-            li.style.display = "none";
-            console.log("active link found");
-        }
-    });
 });
+
 async function fetchPosts(endpoint, page = 1, limit = 16, ajax = 1) {
     try {
         const url = window.location.origin + endpoint + (endpoint.includes('?') ? '&' : '?') + 'page=' + page + '&limit=' + limit + '&ajax=' + ajax;
-        console.log(url);
+        // console.log(url);
         const response = await fetch(url);
         const data = await response.text();
         const dataStr = data.replaceAll('\n', '').replaceAll('&quot;', '"');
         const jsonData = JSON.parse(dataStr);
-        console.log(jsonData);
+        // console.log(jsonData);
         return jsonData;
     }
     catch (error) {
